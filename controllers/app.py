2a9e39745a3261b69eb8fa18e5c9c7a8d2bc6f16
spelunky-forever <haydenpigko@gmail.com>
2025-10-28 23:05:42 +0800

    feat:Add id,add delete_book,add delete_result.But adjust_content is not yet done.

--- controllers/app.py
+++ controllers/app.py
@@ -7,6 +7,12 @@ from models.library import LibraryModel
 
 app = Flask(__name__, template_folder="../views")
 library = LibraryModel()
+# Initialized library
+library.add_book("The Great Gatsby", "F. Scott Fitzgerald")
+library.add_book("To Kill a Mockingbird", "Harper Lee")
+library.add_book("1984", "George Orwell")
+library.add_book("Python Programming", "John Zelle")
+library.add_book("Fluent Python", "Luciano Ramalho")
 
 @app.route("/")
 def home():
@@ -17,11 +23,13 @@ def home():
 def search():
     # Get search keyword from user
     keyword = request.args.get("keyword", "")
-    search_type = request.args.get("type", "title")
+    search_type = request.args.get("type", "bid")
     if search_type == "author":
         results = library.search_by_author(keyword)
-    else:
+    elif search_type == "title":
         results = library.search_by_title(keyword)
+    else:
+        results = library.search_by_bid(keyword)
     return render_template("results.html", keyword=keyword, books=results)
 
 
@@ -33,6 +41,15 @@ def add_book():
         library.add_book(title, author)
         return redirect(url_for("home"))
     return render_template("add_book.html")
+
+@app.route("/delete_book", methods=["GET", "POST"])
+def delete_book():
+    if request.method == "POST":
+        bid = request.form.get("id")
+        deleted_book = library.delete_book(bid)
+        return render_template("delete_result.html",deleted_book=deleted_book ,id=bid)
+    return render_template("delete_book.html")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models/library.py
+++ models/library.py
@@ -1,27 +1,50 @@
+import random
+
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, title, author, bid):
         self.title = title
         self.author = author
+        self.bid = bid
 
 
 class LibraryModel:
     def __init__(self):
         # Pretend this is our small book database
-        self.books = [
-            Book("The Great Gatsby", "F. Scott Fitzgerald"),
-            Book("To Kill a Mockingbird", "Harper Lee"),
-            Book("1984", "George Orwell"),
-            Book("Python Programming", "John Zelle"),
-            Book("Fluent Python", "Luciano Ramalho")
-        ]
+        self.books = []
+        self.bid_list = []
 
     def search_by_title(self, title):
         return [book for book in self.books if title.lower() in book.title.lower()]
     
     def search_by_author(self, author):
         return [book for book in self.books if author.lower() in book.author.lower()]
+
+    def search_by_bid(self, bid):
+        return [book for book in self.books if bid in book.bid]
     
     def add_book(self, title, author):
-        new_book = Book(title, author)
+        while(1):
+            try_bid=str(random.randint(1,10000))
+            if try_bid not in self.bid_list:
+                self.bid_list.append(try_bid)
+                break
+        new_book = Book(title,author,try_bid)
         self.books.append(new_book)
         return new_book
+    
+    def delete_book(self,bid):
+        for index, book in enumerate(self.books):
+            if book.bid==bid:
+                self.bid_list.pop(index)
+                return self.books.pop(index)
+        return False
+
+    def adjust_content(self, new_title, new_author, bid):
+        for index, book in enumerate(self.books):
+            if book.bid==bid:
+                new_book=Book(new_title, new_author, bid)
+                self.books[index]=new_book
+                return new_book
+        return False
+        
+        
